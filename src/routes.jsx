c74a62c0802c2312d31358ef74f7131d825d703d
Gaurav Kumar <gauravkr2179@gmail.com>
2025-05-17 11:15:57 +0530

    fix issue

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,30 +1,33 @@
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignupPage';
 import ExplorePage from './pages/explorePage';
-import BookshelfPage from './pages/BookshelfPage';
-import TimelinePage from './pages/TimelinePage';
-import ProtectedRoute from  './utlis/ProtectedRoute'
+import ProtectedRoute from '../src/utlis/ProtectedRoute';
+
+// Lazy load heavy pages for code splitting
+const BookshelfPage = lazy(() => import('./pages/BookshelfPage'));
+const TimelinePage = lazy(() => import('./pages/TimelinePage'));
 
 const AppRoutes = () => {
   return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/explore" element={<ExplorePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
 
-    <Routes>
+        {/* Protected Routes */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/bookshelf" element={<BookshelfPage />} />
+          <Route path="/timeline" element={<TimelinePage />} />
+        </Route>
 
-      <Route path="/" element={<HomePage />} />
-      <Route path="/explore" element={<ExplorePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-
-      {/* Protected Routes */}
-      <Route element={<ProtectedRoute />}>
-        <Route path="/bookshelf" element={<BookshelfPage />} />
-        <Route path="/timeline" element={<TimelinePage />} />
-      </Route>
-
-      {/* 404 fallback can go here */}
-    </Routes>
+        {/* 404 fallback can be added here */}
+      </Routes>
+    </Suspense>
   );
 };
 
